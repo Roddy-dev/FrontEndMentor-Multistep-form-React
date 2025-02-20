e6ccbcbf0565a344b392ad476676ee3fd4dfb638
Roddy-dev <roddywebdev@gmail.com>
2025-02-20 12:26:24 +0000

    change base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FrontEndMentor-Multistep-form-React",
+  base: "/FrontEndMentor-Multistep-form-React/",
 });
